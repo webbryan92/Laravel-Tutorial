50a5c39e0db060a2f841db560fe4e58b7dc3257a
Ryan Webb <webbryan92@gmail.com>
2019-05-06 20:52:52 -0400

    data passing to views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,14 +10,13 @@
 
 <body>
 
-    @yield('content')
-
-
     <ul>
         <li><a href="""/">Home</a></li>
         <li><a href="/about">About Us</a></li>
         <li><a href="/contact">Contact</a></li>
     </ul>
+
+    @yield('content')
 </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,13 @@
 @section('title', 'Laracasts')
 
 @section('content')
-    <h1>My First Website!</h1>
+    <h1>My {{ $foo }} Website!</h1>
+<ul>
+    <?php foreach($tasks as $task) : ?>
+        <li><?= $task; ?></li>
+    <?php endforeach; ?>
+    @foreach($tasks as $task)
+        <li>{{ $task  }}</li>
+    @endforeach
+</ul>
 @endsection
